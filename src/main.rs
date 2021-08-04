70c177a9c0c51b7fbae2fef864b0b6473309d375
smudge <nathan@ngriffith.com>
2021-08-04 14:31:27 -0400

    Iterate through entire source and target and count files (for now)

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ extern crate getopts;
 
 use getopts::Options;
 use std::env;
+use std::fs;
+use std::io;
 use std::path::Path;
 
 fn print_usage(opts: Options) {
@@ -26,13 +28,13 @@ fn main() {
         matches.opt_str("t"),
         matches.opt_present("h"),
     ) {
-        (Some(s), Some(t), false) if Path::new(&s).exists() && Path::new(&t).exists() => {
+        (Some(s), Some(t), false) if Path::new(&s).is_dir() && Path::new(&t).is_dir() => {
             run(Path::new(&s), Path::new(&t))
         }
-        (Some(s), Some(_), false) if !Path::new(&s).exists() => {
+        (Some(s), Some(_), false) if !Path::new(&s).is_dir() => {
             eprintln!("Unable to find source dir: {}", s)
         }
-        (Some(_), Some(t), false) if !Path::new(&t).exists() => {
+        (Some(_), Some(t), false) if !Path::new(&t).is_dir() => {
             eprintln!("Unable to find target dir: {}", t)
         }
         (_, _, _) => print_usage(opts),
@@ -40,9 +42,21 @@ fn main() {
 }
 
 fn run(source: &Path, target: &Path) {
-    println!(
-        "source: {}, target: {}",
-        source.to_str().unwrap(),
-        target.to_str().unwrap()
-    );
+    let source_count = count_files(source, 0);
+    let target_count = count_files(target, 0);
+    println!("source: {}", source_count.unwrap());
+    println!("target: {}", target_count.unwrap());
+}
+
+fn count_files(dir: &Path, mut count: u64) -> io::Result<u64> {
+    for entry in fs::read_dir(dir)? {
+        let entry = entry?;
+        let path = entry.path();
+        if path.is_dir() {
+            count = count_files(&path, count)?;
+        } else if path.is_file() {
+            count += 1;
+        }
+    }
+    Ok(count)
 }
