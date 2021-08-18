ce65e11c9bfaf64a2f6e7c08efee5607bf3d1869
smudge <nathan@ngriffith.com>
2021-08-17 22:19:45 -0400

    Group files by size and print out soft duplicates

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,11 @@ extern crate spinner;
 
 use getopts::Options;
 use spinner::{SpinnerBuilder, SpinnerHandle};
+use std::collections::HashMap;
 use std::env;
 use std::fs;
 use std::io;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 fn print_usage(opts: Options) {
     let args: Vec<String> = env::args().collect();
@@ -44,10 +45,23 @@ fn main() {
 }
 
 fn run(source: &Path, target: &Path) {
+    let mut files_by_size: HashMap<u64, Vec<PathBuf>> = HashMap::new();
     let sp = SpinnerBuilder::new("Discovering source files... (0)".into()).start();
-    let source_count = count_files(source, 0, &sp, "Discovering source files...");
+    let source_count = count_files(
+        source,
+        0,
+        &sp,
+        "Discovering source files...",
+        &mut files_by_size,
+    );
     sp.update("Discovering target files... (0)".into());
-    let target_count = count_files(target, 0, &sp, "Discovering target files...");
+    let target_count = count_files(
+        target,
+        0,
+        &sp,
+        "Discovering target files...",
+        &mut files_by_size,
+    );
     sp.update(" ".into());
     sp.close();
     println!(
@@ -55,15 +69,34 @@ fn run(source: &Path, target: &Path) {
         source_count.unwrap(),
         target_count.unwrap()
     );
+    println!("Files by size:");
+    for key in files_by_size.keys() {
+        let count = files_by_size.get(key).unwrap().len();
+        if count > 1 {
+            println!("{}: {}", key, count);
+        }
+    }
 }
 
-fn count_files(dir: &Path, mut count: u64, sp: &SpinnerHandle, prefix: &str) -> io::Result<u64> {
+fn count_files(
+    dir: &Path,
+    mut count: u64,
+    sp: &SpinnerHandle,
+    prefix: &str,
+    files_by_size: &mut HashMap<u64, Vec<PathBuf>>,
+) -> io::Result<u64> {
     for entry in fs::read_dir(dir)? {
         let entry = entry?;
         let path = entry.path();
         if path.is_dir() {
-            count = count_files(&path, count, sp, prefix)?;
+            count = count_files(&path, count, sp, prefix, files_by_size)?;
         } else if path.is_file() {
+            let size = fs::metadata(path.clone())?.len();
+            if !files_by_size.contains_key(&size) {
+                files_by_size.insert(size, Vec::new());
+            }
+            let list = files_by_size.get_mut(&size).unwrap();
+            list.push(path.clone());
             count += 1;
         }
     }
